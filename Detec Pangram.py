bbd01bc1cee1b30e946b474641d3197d27c9bdfb
Floruta Raul <floruta.raul25@gmail.com>
2021-11-22 21:57:01 +0200

    Reorganised the folders of Python

--- Detec Pangram.py
+++
@@ -1,7 +0,0 @@
-import string
-
-def is_pangram(s: str):
-    no_dubs = set(s.lower())
-    return all(item in no_dubs for item in string.ascii_lowercase)
-
-print(is_pangram("The quick, brown fox jumps over the lazy dog!"))
--- Friend or Foe.py
+++
@@ -1,9 +0,0 @@
-# Make a program that filters a list of strings and returns a list with only your friends name in it.
-#
-# If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
-#
-# Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
-def friend(x):
-    return [elem for elem in x if len(elem)==4]
-
-print(friend(["Ryan", "Kieran", "Jason", "Yous"]))
